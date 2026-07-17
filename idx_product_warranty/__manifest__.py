9bf873c5863bea5c12f8294f0077faff0ee51ac1
kingsmvp2 <kingsmvp2@gmail.com>
2026-07-17 16:55:01 +0800

    產品增加保固月數欄位

---
+++ idx_product_warranty/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': 'Product Warranty',
+    'version': '17.0.1.0.0',
+    'depends': ['product'],
+    'data': [
+        'views/product_template_views.xml',
+    ],
+    'license': 'LGPL-3',
+}
