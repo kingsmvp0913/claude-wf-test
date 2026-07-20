8c9cc486e1f791b88fd2aca6f917444672a935e5
kingsmvp2 <kingsmvp2@gmail.com>
2026-07-20 09:24:12 +0800

    客戶業務負責人並帶入報價單

---
+++ idx_partner_sales_rep/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': 'Partner Sales Rep',
+    'version': '17.0.1.0.0',
+    'depends': ['sale'],
+    'data': [
+        'views/res_partner_views.xml',
+    ],
+    'license': 'LGPL-3',
+}
