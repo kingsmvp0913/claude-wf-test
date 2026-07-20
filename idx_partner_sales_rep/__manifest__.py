96a470ea6cc0ccc01f656ba567edaba15b1b0b73
kingsmvp2 <kingsmvp2@gmail.com>
2026-07-20 09:42:40 +0800

    [idx_partner_sales_rep]: 新增 tour E2E 測試

--- idx_partner_sales_rep/__manifest__.py
+++ idx_partner_sales_rep/__manifest__.py
@@ -5,5 +5,11 @@
     'data': [
         'views/res_partner_views.xml',
     ],
+    'assets': {
+        'web.assets_tests': [
+            'idx_partner_sales_rep/static/tests/tours/partner_sales_rep_tour.js',
+            'idx_partner_sales_rep/static/tests/tours/sale_order_sales_rep_tour.js',
+        ],
+    },
     'license': 'LGPL-3',
 }
